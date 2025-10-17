8642f575d4dd2817a316ac5549d15ebefa01c063
thedevnaath <thedevnaath@gmail.com>
2025-10-17 15:01:20 +0530

    Update TopicBox.jsx

--- src/components/TopicBox.jsx
+++ src/components/TopicBox.jsx
@@ -1,37 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { db } from "../firebase";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 
-export default function TopicBox({ topic, user }) {
-  const [vote, setVote] = useState(null);
-  const [percentages, setPercentages] = useState({ agree: 0, disagree: 0 });
-
-  useEffect(() => {
-    // TODO: fetch real-time vote percentages from Firestore
-  }, [topic]);
-
-  const handleVote = async side => {
-    setVote(side);
+export default function TopicBox({ topic, user, openChat }) {
+  const vote = async (side) => {
     await setDoc(doc(db, "votes", `${topic.id}_${user.uid}`), {
       topicId: topic.id,
       userId: user.uid,
       voteSide: side
     });
-    // TODO: Update percentages in real-time
+    openChat(topic); // Navigate to chatroom after vote
   };
 
   return (
     <div className="topic-box">
-      <h2>{topic.topicText}</h2>
-      <div className="buttons">
-        <button onClick={() => handleVote("Agree")} className="agree-button">Agree</button>
-        <button onClick={() => handleVote("Disagree")} className="disagree-button">Disagree</button>
-      </div>
-      <div className="percentages">
-        <div className="agree-percentage">{percentages.agree}%</div>
-        <div className="disagree-percentage">{percentages.disagree}%</div>
+      <p>{topic.topicText}</p>
+      <div className="vote-buttons">
+        <button onClick={() => vote("Agree")} className="agree">Agree</button>
+        <button onClick={() => vote("Disagree")} className="disagree">Disagree</button>
       </div>
     </div>
   );
 }
-
