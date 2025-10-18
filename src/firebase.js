705e278ec50af16abc0389e7b94a561aec7e9f4c
thedevnaath <thedevnaath@gmail.com>
2025-10-18 20:16:24 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
 // src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyA2c7lO8VHi956b-qekrRkgSQuuGW-BfM8",
   authDomain: "clashchatz.firebaseapp.com",
@@ -11,16 +11,11 @@ const firebaseConfig = {
   storageBucket: "clashchatz.firebasestorage.app",
   messagingSenderId: "75470322957",
   appId: "1:75470322957:web:89e5e370e17693e3f1c444",
-  measurementId: "G-LPSSLW4JSN",
-  databaseURL: "https://clashchatz-default-rtdb.firebaseio.com"
+  measurementId: "G-LPSSLW4JSN"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Auth & Firestore
 export const auth = getAuth(app);
-export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
-
+export const db = getFirestore(app);
 export default app;
