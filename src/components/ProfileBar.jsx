b21c0608852e668aea0fbdd23974aeeeceee2c61
thedevnaath <thedevnaath@gmail.com>
2025-10-18 20:21:15 +0530

    Update ProfileBar.jsx

--- src/components/ProfileBar.jsx
+++ src/components/ProfileBar.jsx
@@ -1,10 +1,11 @@
+// src/components/ProfileBar.jsx
 import React from "react";
 
-export default function ProfileBar({ user }) {
+export default function ProfileBar({ user }){
   return (
     <div className="profile-bar">
-      <h3>{user.displayName}</h3>
+      <img src={user.photoURL} alt="avatar" style={{width:36,height:36,borderRadius:18,verticalAlign:"middle",marginRight:8}} />
+      <strong>{user.displayName}</strong>
     </div>
   );
 }
-
