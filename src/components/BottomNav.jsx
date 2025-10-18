565d5aac8374de7ff52c4e68e46343c9ef7e5904
thedevnaath <thedevnaath@gmail.com>
2025-10-18 20:22:09 +0530

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,11 +1,12 @@
+// src/components/BottomNav.jsx
 import React from "react";
 
-export default function BottomNav({ setScreen }) {
+export default function BottomNav({ setScreen }){
   return (
     <div className="bottom-nav">
-      <button onClick={() => setScreen("feed")}>Home</button>
-      <button onClick={() => setScreen("add-topic")}>+</button>
+      <button onClick={()=> setScreen("feed")}>🏠</button>
+      <button onClick={()=> setScreen("add")}>➕</button>
+      <button onClick={()=> setScreen("leaderboard")}>🏆</button>
     </div>
   );
 }
-
